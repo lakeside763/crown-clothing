0f2e71f4b018f14db889dcda39aff2e635043453
lakeside763 <lakeside763@gmail.com>
2022-01-10 17:49:02 +0100

    chore: setup collection-preview and item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/Homepage.component';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import Homepage from "./pages/homepage/Homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
